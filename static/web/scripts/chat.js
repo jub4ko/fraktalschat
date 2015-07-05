d8f4f3e43b1dea7e577f4394014509a5d4a64076
ddezeeuw <ddezeeuw@gmail.com>
2015-07-05 10:59:21 +0200

    socket.connect update

--- static/web/scripts/chat.js
+++ static/web/scripts/chat.js
@@ -1,8 +1,7 @@
 'use strict';
 
 jQuery(function ($) {
-    //var socket = io.connect('https://serene-mesa-8502.herokuapp.com/');
-    var socket = io.connect();
+    var socket = io.connect("https://serene-mesa-8502.herokuapp.com/");
 
     var $avatar = $('#avatar');
     var $addForm = $('#add-user');
